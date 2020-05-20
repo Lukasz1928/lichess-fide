36fb177866abb15d441c8adabdc1e5bb3f0be408
Lukasz1928 <Lukasz19281@gmail.com>
2020-05-20 17:07:47 +0200

    Added basic data handling

---
+++ src/data.py
@@ -0,0 +1,15 @@
+
+def get_db_filename(year, month):
+    return 'db-{}-{:02d}.pgn.bz2'.format(year, month)
+
+
+def get_db_filepath(year, month):
+    return '../data/{}'.format(get_db_filename(year, month))
+
+
+def get_names_filepath(year, month):
+    return '../data/names-{}-{:02d}.txt'.format(year, month)
+
+
+def get_ranking_filepath(year, month):
+    return '../data/rankings-{}-{:02d}.csv'.format(year, month)
--- src/db.py
+++ src/db.py
@@ -2,23 +2,13 @@ import bz2
 import regex
 from wget import download
 
-
-def _get_db_filename(year, month):
-    return 'db-{}-{:02d}.pgn.bz2'.format(year, month)
-
-
-def _get_db_filepath(year, month):
-    return '../data/{}'.format(_get_db_filename(year, month))
-
-
-def _get_names_filepath(year, month):
-    return '../data/names-{}-{:02d}.txt'.format(year, month)
+from src.data import get_db_filepath, get_names_filepath
 
 
 def download_database(year, month):
     filename = "lichess_db_standard_rated_{}-{:02d}.pgn.bz2".format(year, month)
     url = "https://database.lichess.org/standard/{}".format(filename)
-    download(url, out=_get_db_filepath(year, month))
+    download(url, out=get_db_filepath(year, month))
 
 
 def _get_name(line):
@@ -33,8 +23,8 @@ def _get_name(line):
 
 def process_database(year, month):
     names = set()
-    with open(_get_names_filepath(year, month), 'w+') as namesf:
-        with bz2.BZ2File(_get_db_filepath(year, month)) as f:
+    with open(get_names_filepath(year, month), 'w+') as namesf:
+        with bz2.BZ2File(get_db_filepath(year, month)) as f:
             for line in f:
                 n = _get_name(line.decode('utf-8'))
                 if n is not None and n not in names:
