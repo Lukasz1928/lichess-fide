12dff93a90a14f17b628a47c6b62e0f2b5c0b54b
Lukasz1928 <Lukasz19281@gmail.com>
2020-07-01 12:02:11 +0200

    React page test

---
+++ docs/precache-manifest.416933ed6dcf351861e0f607d909baa4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af2ad4858e8e6bcc8b54f3d50e527428",
+    "url": "/lichess-fide/index.html"
+  },
+  {
+    "revision": "0bcc2878acc11a757fc3",
+    "url": "/lichess-fide/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1eea3c39b02ea2d4dbea",
+    "url": "/lichess-fide/static/js/2.940b95c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lichess-fide/static/js/2.940b95c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bcc2878acc11a757fc3",
+    "url": "/lichess-fide/static/js/main.31c59dab.chunk.js"
+  },
+  {
+    "revision": "c112609131bf63a81abb",
+    "url": "/lichess-fide/static/js/runtime-main.007d687f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lichess-fide/static/media/logo.5d5d9eef.svg"
+  }
+]);
